79fb8791147944da9b23c298afae2b2eb730a0d2
McKooG <koliandru@mail.ru>
2022-04-19 20:39:55 +0300

    AddStationFuels

--- StationsFuels.cs
+++ StationsFuels.cs
@@ -1,10 +1,32 @@
 namespace ProjectTestAPI_1
 {
-    public class StationsFuels
+    public class StationFuels_JsonByStation
     {
+        public StationFuels_JsonByStation(ulong id,ulong stationId,ulong fuelId)
+        {
+            Id = id;
+            StationId = stationId;
+            FuelId = fuelId;
+        }
+        public ulong Id {get; set;}
         public ulong StationId { get; set; }
         
-        public List<ulong> FuelList { get; set; } 
+        public ulong FuelId { get; set; } 
         
+    }
+
+    public class StationFuels_JsonByFuel 
+    {
+        public StationFuels_JsonByFuel(ulong id,ulong fuelId,ulong stationId)
+        {
+            Id = id;
+            FuelId = fuelId;
+            StationId = stationId;            
+        }
+        public ulong Id {get; set;}
+        public ulong FuelId { get; set; } 
+        public ulong StationId { get; set; }
+
+
     }
 }
